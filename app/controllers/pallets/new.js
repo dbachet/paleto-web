6186239baea16bf86b6d0d5e4adec54811b51fea
Damien Bachet <dbachet@gmail.com>
2016-04-05 01:16:49 +0200

    Add error messages

--- app/controllers/pallets/new.js
+++ app/controllers/pallets/new.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   currentSession: Ember.inject.service(),
+
   actions: {
     create() {
-      this.get('model').save();
-      this.transitionToRoute('pallets.index');
+      var _this = this;
+      this.get('model').save().then(function() {
+        _this.transitionToRoute('pallets.index');
+      });
     }
   }
 });
